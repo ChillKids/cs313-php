1c87279680ff3fb5ba66ca8503a5684aae4a3682
JackKit <chillkit@users.noreply.github.com>
2019-10-01 16:39:25 -0600

    yea

--- web/Prove02/web/prove02/prove02.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-
-    
-    // when the form is submitted
-    $('#contact-form').on('submit', function (e) {
-    
-        alert("Summited");
-    }
-)})
--- web/Prove02/web/prove02/prove02.php
+++
@@ -1,6 +0,0 @@
-<?php
-date_default_timezone_set('America/Phoenix'); // CDT
-
-$current_date = date('d/m/Y');
-
-echo "<p>".$current_date."</p>";?>
