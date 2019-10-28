f4b145514543606428b6027452fb0f3e38e7f861
JackKit <chillkit@users.noreply.github.com>
2019-10-28 03:28:19 -0600

    hgh

--- web/INote/delete.php
+++ web/INote/delete.php
@@ -3,7 +3,7 @@ require('dbconnection.php');
 
 $note_id = $_POST['note_id'];
 $user_id = $_POST['user_id'];
-/*
+
 $stmt = $db->prepare("DELETE FROM note WHERE id = :note_id");
 $stmt->bindValue(':note_id', $note_id, PDO::PARAM_INT);
 $stmt->execute();
@@ -14,4 +14,4 @@ $stmt->bindValue(':note_id', $note_id, PDO::PARAM_INT);
 $stmt->execute();
 $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-header("url=AddNote.php?id=" . $user_id);*/
+header("refresh:3; url=AddNote.php?id=" . $user_id);
