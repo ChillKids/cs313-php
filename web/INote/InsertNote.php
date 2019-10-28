11ee423c9d1d8d6e7e613352d495108f05effb2b
JackKit <chillkit@users.noreply.github.com>
2019-10-28 02:07:38 -0600

    asdfasdf

--- web/INote/InsertNote.php
+++ web/INote/InsertNote.php
@@ -16,7 +16,7 @@ function addNote($db, $note, $user_id, $module_id, $class_id)
     $stmt->fetchAll(PDO::FETCH_ASSOC);
     $note_id = $db->lastInsertId();
 
-    echo 'Note id:'.$note_id.'<br>';
+    echo 'Note id:' . $note_id . '<br>';
     $stmt = $db->prepare("INSERT INTO module_note (module_id, note_id) VALUES (:module_id, :note_id)");
     $stmt->bindValue(':module_id', $module_id, PDO::PARAM_INT);
     $stmt->bindValue(':note_id', $note_id, PDO::PARAM_INT);
@@ -24,7 +24,7 @@ function addNote($db, $note, $user_id, $module_id, $class_id)
     $stmt->fetchAll(PDO::FETCH_ASSOC);
     echo 'Successfully Saved! It will redirect to note in 3 sec<br/>';
 
-    header("refresh:3; url=AddNode.php?id=".$user_id);
+    header("refresh:3; url=AddNote.php?id=" . $user_id);
 }
 
 function addModule($db, $module_name, $user_id, $class_id, $note)
@@ -63,22 +63,22 @@ function addClass($db, $user_id, $class_name, $module_name, $note)
 try {
     $statement = $db->query("SELECT id FROM class WHERE name = '$class_name'");
     $class_id = $statement->fetchAll(PDO::FETCH_ASSOC);
-        echo "check class<br>";
-   if (!empty($class_id)) {
-    echo "class exist<br>";
+    //  echo "check class<br>";
+    if (!empty($class_id)) {
+        // echo "class exist<br>";
         $statement = $db->query("SELECT id FROM module WHERE name = '$module_name'");
         $module_id = $statement->fetchAll(PDO::FETCH_ASSOC);
-        echo "check module<br>";
+        //   echo "check module<br>";
         if (!empty($module_id)) {
-            echo "module exist<br>";
+            //     echo "module exist<br>";
             addNote($db, $note, $user_id, $module_id[0]['id'], $class_id[0]['id']);
         } else {
-            echo "module does not exist<br>";
-           // addModule($db, $module_name, $user_id, $class_id[0]['id'], $note);
+            //   echo "module does not exist<br>";
+            addModule($db, $module_name, $user_id, $class_id[0]['id'], $note);
         }
     } else {
-        echo "class does not exist<br>";
-       // addClass($db, $user_id, $class_name, $module_name, $note);
+        //     echo "class does not exist<br>";
+        // addClass($db, $user_id, $class_name, $module_name, $note);
     }
 } catch (PDOException $ex) {
     echo 'Error!: ' . $ex->getMessage();
