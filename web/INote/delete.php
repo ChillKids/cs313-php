0e62421ba2795146f12bd118ef60fd75ac398411
JackKit <chillkit@users.noreply.github.com>
2019-10-28 03:38:25 -0600

    asdas

--- web/INote/delete.php
+++ web/INote/delete.php
@@ -4,15 +4,17 @@ require('dbconnection.php');
 $note_id = $_GET['note_id'];
 $user_id = $_GET['user_id'];
 
+$stmt = $db->prepare("DELETE FROM module_note WHERE note_id = :note_id");
+$stmt->bindValue(':note_id', $note_id, PDO::PARAM_INT);
+$stmt->execute();
+$stmt->fetchAll(PDO::FETCH_ASSOC);
+
 $stmt = $db->prepare("DELETE FROM note WHERE id = :note_id");
 $stmt->bindValue(':note_id', $note_id, PDO::PARAM_INT);
 $stmt->execute();
 $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-$stmt = $db->prepare("DELETE FROM module_note WHERE note_id = :note_id");
-$stmt->bindValue(':note_id', $note_id, PDO::PARAM_INT);
-$stmt->execute();
-$stmt->fetchAll(PDO::FETCH_ASSOC);
+
 
 echo 'Successfully Deleted<br>';
 
