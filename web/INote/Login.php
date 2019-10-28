4df388998a9fa5d034a46e3277a05f10458c244e
JackKit <chillkit@users.noreply.github.com>
2019-10-27 19:58:21 -0600

    debug id

--- web/INote/Login.php
+++ web/INote/Login.php
@@ -3,16 +3,16 @@ require ('dbconnection.php');
                 $name = htmlspecialchars($_POST['name']);
                 $password = htmlspecialchars($_POST['password']);
 
-                $stmt = $db->prepare("SELECT id FROM user_profile WHERE (name = :name AND password = :password) ");
+                $stmt = $db->prepare("SELECT * FROM user_profile WHERE (name = :name AND password = :password) ");
                 $stmt->bindValue(':password', $password, PDO::PARAM_STR);
                 $stmt->bindValue(':name', $name, PDO::PARAM_STR);
                 $stmt->execute();
-                $id = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                $row = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-                if (!empty($id)) {
+                if (!empty($row)) {
                     echo 'login successfully! id:';
-                    echo $id['id'];
-                    header("refresh:1; url=AddNote.php?id=" . $id['id']);
+                    echo $row['id'];
+                    header("refresh:1; url=AddNote.php?id=" . $row['id']);
                 } else {
                     echo 'Username or Password Wrong!';
                     header("refresh:1; url=INotedb.php");
