d884ae1fb3a00270190569ece8bcb5937280893e
JackKit <chillkit@users.noreply.github.com>
2019-10-29 16:10:26 -0600

    asdlfkjasdfl

--- web/TA07/signin.php
+++ web/TA07/signin.php
@@ -41,7 +41,7 @@ $username = $_POST['username'];
 $password = $_POST['password'];
 
 try {
-    $stored_password = db->query("SELECT password FROM users WHERE username=$username");
+    $stored_password = db->query("SELECT password FROM users WHERE username = 'jack' ");
   //  $hashed_password = password_hash($password, PASSWORD_DEFAULT);
   //  if (password_verify($hashed_password, $stored_password)) {
   //      $newURL = 'success.php';
