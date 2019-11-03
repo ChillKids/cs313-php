ee4c9d4d216589a1a24542fb53d4a29c1d15f58b
JackKit <chillkit@users.noreply.github.com>
2019-11-03 15:32:35 -0700

    password encrpt

--- web/INote/Login.php
+++ web/INote/Login.php
@@ -3,13 +3,16 @@ require ('dbconnection.php');
                 $name = htmlspecialchars($_POST['name']);
                 $password = htmlspecialchars($_POST['password']);
 
-                $stmt = $db->prepare("SELECT * FROM user_profile WHERE (name = :name AND password = :password) ");
+                $stmt = $db->prepare("SELECT password FROM user_profile WHERE (name = :name AND password = :password) ");
                 $stmt->bindValue(':password', $password, PDO::PARAM_STR);
                 $stmt->bindValue(':name', $name, PDO::PARAM_STR);
                 $stmt->execute();
                 $row = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-                if (!empty($row)) {
+                $stored_password = $row[0]['password'];
+                password_verify($password, $stored_password);
+
+                if (!empty($row) && password_verify($password, $stored_password)) {
                     echo 'login successfully!';
                     header("refresh:1; url=AddNote.php?id=" . $row[0]['id']);
                     die();
--- web/INote/SignUpSql.php
+++ web/INote/SignUpSql.php
@@ -2,7 +2,7 @@
 $name = htmlspecialchars($_POST['name']);
 $password = htmlspecialchars($_POST['password']);
 require('dbconnection.php');
-
+$hashed_password = password_hash($password, PASSWORD_DEFAULT);
 $statement = $db->query("SELECT id FROM user_profile WHERE name = '$name'");
                 $results = $statement->fetchAll(PDO::FETCH_ASSOC);
 
@@ -14,7 +14,7 @@ $statement = $db->query("SELECT id FROM user_profile WHERE name = '$name'");
                 } else { 
                     try {
                         $stmt = $db->prepare("INSERT INTO user_profile (name, password) VALUES (:name, :password)");
-                        $stmt->bindValue(':password', $password, PDO::PARAM_STR);
+                        $stmt->bindValue(':password', $hashed_password, PDO::PARAM_STR);
                         $stmt->bindValue(':name', $name, PDO::PARAM_STR);
                         $stmt->execute();
                         $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
