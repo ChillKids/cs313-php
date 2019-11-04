a9f6fc5da04d944f21c48e0513ed7b918db10c0a
JackKit <chillkit@users.noreply.github.com>
2019-11-03 17:57:17 -0700

    login redirect

--- web/INote/AddNote.php
+++ web/INote/AddNote.php
@@ -1,7 +1,7 @@
 <?php
 require('dbconnection.php');
 session_start();
-if ($_SESSION['id'] === ""){
+if ($_SESSION['id'] == ""){
     echo "It will redirect you back to the login page";
     header("refresh:3; url=INotedb.php");
 }
